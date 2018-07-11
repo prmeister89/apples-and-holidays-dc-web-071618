6e8abf0ce6253dd7f8c61f9c61d60bccf81ee7c8
Peter Meister <prmeister89@gmail.com>
2018-07-11 18:44:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
         new_array.each do |word|
           word.capitalize!
         end
-        new_array
+        new_array.join
         
     
       end
