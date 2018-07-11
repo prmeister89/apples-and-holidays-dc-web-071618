b93e04d3a16175dfac3c4ea8257808a67a21354b
Peter Meister <prmeister89@gmail.com>
2018-07-11 18:24:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   
-  holiday_hash.each do |season, holiday|
+  holiday_hash.each do |season, holidays|
     season_string = season.to_s + ":"
     season_string.capitalize!
     
